efaa78e4ab3fe64f275674a8d76b4a01f9684e96
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-23 09:28:50 +0700

    Mengisi file UIpertama dengan funsi Activitas Pertama, Column

--- app/src/main/java/com/example/pertemuan4/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/pertemuan4/ui/theme/UIpertama.kt
@@ -2,3 +2,8 @@ package com.example.pertemuan4.ui.theme
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
+    Column(modifier = Modifier.padding(top = 100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+}

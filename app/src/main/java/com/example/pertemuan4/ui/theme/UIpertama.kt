00ea0c3dbe45b6b9aa820f4b8bf237c85b4d9ede
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-23 09:30:45 +0700

    Mengisi file UIpertama menambahkah Import

--- app/src/main/java/com/example/pertemuan4/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/pertemuan4/ui/theme/UIpertama.kt
@@ -14,6 +14,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){

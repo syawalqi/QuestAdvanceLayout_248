60df470825b1724531e697f05d4c55c7ff850d57
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-23 09:31:16 +0700

    Mengisi file UIpertama menambahkah spacer

--- app/src/main/java/com/example/pertemuan4/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/pertemuan4/ui/theme/UIpertama.kt
@@ -41,6 +41,15 @@ fun ActivitasPertama(modifier: Modifier){
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
+        Spacer(modifier = Modifier.height(height = 25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+
 
     }
 }
